8a2ca22b1b8260b75d90bff44ff6e599677c8220
zekai <zekaicai@usc.edu>
2021-03-09 19:13:28 -0800

    Pause music when puase the game

--- Assets/Scripts/HelpReturn.cs
+++ Assets/Scripts/HelpReturn.cs
@@ -10,12 +10,22 @@ public class HelpReturn : MonoBehaviour
     }
     public void PauseGame()
     {
+        AudioSource bgm = Camera.main.GetComponent<AudioSource>();
+        if (bgm != null)
+        {
+            bgm.Pause();
+        }
         Time.timeScale = 0f;
     }
     public void ResumeToScene()
     {
         GameObject.Find("Canvas/Buttons/Rules").SetActive(false);
         Time.timeScale = 1f;
+        AudioSource bgm = Camera.main.GetComponent<AudioSource>();
+        if (bgm != null)
+        {
+            bgm.Play();
+        }
     }
     public void ReturnToMainMenu()
     {
