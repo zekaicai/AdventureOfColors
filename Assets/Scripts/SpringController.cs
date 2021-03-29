8f2c8559a6529c504cab886c57190f9d5b5b1fcf
yt <ytyuyunting@gmail.com>
2021-03-29 17:25:58 +0800

    add first spring's animation

--- Assets/Scripts/SpringController.cs
+++ Assets/Scripts/SpringController.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class SpringController : MonoBehaviour
 {
+    protected Animator anim;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+//Debug.Log("Tag is ");
+        anim = GetComponent<Animator>();
     }
+    private void Update()
+    {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    // 碰撞开始
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //Debug.Log("Tag is 1");
+        anim.SetBool("Release", false);
+        anim.SetBool("Compression", true);
+         // 销毁当前游戏物体
+         //Destroy(this.gameObject);
+     }
+ 
+     // 碰撞结束
+     private void OnCollisionExit2D(Collision2D collision)
+     {
+        //Debug.Log("Tag is 2");
+        anim.SetBool("Compression", false);
+        anim.SetBool("Release", true);
+     }
+ 
+     // 碰撞持续中
+    private void OnCollisionStay2D(Collision2D collision)
+     {
+      //  Debug.Log("Tag is 3");
+        
+     }
+    // Vector3 fixPos;
+    // public float elasticValue=0.1f; //弹性系数
+    // Vector3 forceDie;             //力量的方向
+    // Rigidbody2D rig;
+    // private void Start()
+    // {
+    //     fixPos = transform.position;
+    //     rig = GetComponent<Rigidbody2D>();
+    // }
+    //   // Update is called once per frame
+    // private void Update()
+    // {
+    //     Elastic();  
+    // }
+    // void Elastic()
+    // {
+    //     if (rig.velocity == Vector2.zero) return;
+    //     rig.AddForce((fixPos - transform.position) * elasticValue*Time.deltaTime);
+    // }
+
 }
