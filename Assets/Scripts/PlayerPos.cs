9dfb85b99e8a5b8f087f012aef4d0d48ad0fc270
zekai <zekaicai@usc.edu>
2021-04-29 13:41:50 -0700

    Fix start position bug

--- Assets/Scripts/PlayerPos.cs
+++ Assets/Scripts/PlayerPos.cs
@@ -19,10 +19,14 @@ public class PlayerPos : MonoBehaviour
 
         if (currentSceneName != gm.lastSceneName)
         {
+            print("Load Default");
             LoadDefault();
+            gm.lastCheckpointPos = map[currentSceneName];
+            bgm.time = 0;
         }
         else
         {
+            print("Load Master" + gm.lastCheckpointPos);
             transform.position = gm.lastCheckpointPos;
             bgm.time = gm.audioTime;
             bgm.Play();
@@ -34,8 +38,8 @@ public class PlayerPos : MonoBehaviour
     private void AddDefault()
     {
         map.Add("SquareTutorial", new Vector2(-42, -4));
-        map.Add("VodkaDance", new Vector2(-54, -4));
-        map.Add("SquareScene", new Vector2(-23, -3));
+        map.Add("VodkaDance", new Vector2(-53.99f, -4.6f));
+        map.Add("SquareScene", new Vector2(-23, -4.5f));
         map.Add("square02", new Vector2(-70.34f, 5.52f));
         map.Add("circle02", new Vector2(-53.05f, -3.49f));
     }
